d2929a86741d0b4e537aa0465841c23a8faedc3c
Lead with Pius <piusbruce595@gmail.com>
2025-12-10 23:03:52 +0300

    updated config

--- config.java
+++ config.java
@@ -1,3 +1,7 @@
 public class config{
-    
+    public static final init WIDTH = 800;
+    public static final init HEIGHT = 600;
+    public static final init GRAVITY = 0.6;
+    public static final init JUMP_FORCE = -12.0;
+    public static final init GROUND_Y = HEIGHT -100;
 }
