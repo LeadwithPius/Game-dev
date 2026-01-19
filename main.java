a77dac87db775d7a3323a1b04e30fa5d88bdd39e
Siantei Jama <sianteijama@gmail.com>
2026-01-19 13:55:54 +0300

    Update main.java

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 import javafx.application.Application;
+import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
-import javafx,stage.stage; 
 
 public class Main extends Application{
     @Override
